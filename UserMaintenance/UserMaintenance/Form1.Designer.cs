aad576e987cf9f524859764b66e1a6049da48636
podi <podipeti17@gmail.com>
2023-03-07 09:00:22 +0100

    IRF gyak3 - Feladat #9

--- UserMaintenance/UserMaintenance/Form1.Designer.cs
+++ UserMaintenance/UserMaintenance/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.textFullName = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.btnAdd = new System.Windows.Forms.Button();
+            this.btnWrite = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listUsers
@@ -39,7 +40,7 @@
             this.listUsers.FormattingEnabled = true;
             this.listUsers.ItemHeight = 16;
             this.listUsers.Location = new System.Drawing.Point(13, 13);
-            this.listUsers.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listUsers.Margin = new System.Windows.Forms.Padding(4);
             this.listUsers.Name = "listUsers";
             this.listUsers.Size = new System.Drawing.Size(185, 276);
             this.listUsers.TabIndex = 0;
@@ -47,7 +48,7 @@
             // textFullName
             // 
             this.textFullName.Location = new System.Drawing.Point(282, 10);
-            this.textFullName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textFullName.Margin = new System.Windows.Forms.Padding(4);
             this.textFullName.Name = "textFullName";
             this.textFullName.Size = new System.Drawing.Size(201, 22);
             this.textFullName.TabIndex = 1;
@@ -65,7 +66,7 @@
             // btnAdd
             // 
             this.btnAdd.Location = new System.Drawing.Point(206, 68);
-            this.btnAdd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4);
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(277, 45);
             this.btnAdd.TabIndex = 5;
@@ -73,16 +74,28 @@
             this.btnAdd.UseVisualStyleBackColor = true;
             this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
+            // btnWrite
+            // 
+            this.btnWrite.Location = new System.Drawing.Point(209, 244);
+            this.btnWrite.Margin = new System.Windows.Forms.Padding(4);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(277, 45);
+            this.btnWrite.TabIndex = 6;
+            this.btnWrite.Text = "button1";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(498, 309);
+            this.Controls.Add(this.btnWrite);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textFullName);
             this.Controls.Add(this.listUsers);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -96,6 +109,7 @@
         private System.Windows.Forms.TextBox textFullName;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnWrite;
     }
 }
 
--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +20,7 @@ namespace UserMaintenance
             InitializeComponent();
             textFullName.Text = Resource1.FullName; // label1
             btnAdd.Text = Resource1.Add; // button1
+            btnWrite.Text = Resource1.Write;
 
             listUsers.DataSource = users;
             listUsers.ValueMember = "ID";
@@ -32,6 +34,31 @@ namespace UserMaintenance
                 FullName = textFullName.Text,
             };
             users.Add(u);
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        using (StreamWriter sw = new StreamWriter(saveFileDialog.FileName))
+                        {
+                            foreach (User u in users)
+                            {
+                                sw.WriteLine($"{u.ID},{u.FullName}");
+                            }
+                        }
+                        MessageBox.Show("A felhasználók listája sikeresen elmentve.", "Sikeres mentés", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Hiba történt a mentés közben:\n{ex.Message}", "Hiba", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
         }
     }
 }
