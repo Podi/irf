7ebc2c94a9fb9c50d325548ec2f161a117ef47bb
podi <podipeti17@gmail.com>
2023-03-14 09:28:33 +0100

    IRF gyak4 - Feladat #8

--- fd64yt_irf_week4/fd64yt_irf_week4/Form1.cs
+++ fd64yt_irf_week4/fd64yt_irf_week4/Form1.cs
@@ -55,11 +55,18 @@ namespace fd64yt_irf_week4
                 values[counter, 1] = f.Vendor;
                 values[counter, 2] = f.Side;
                 values[counter, 3] = f.District;
-                values[counter, 4] = f.Elevator;
+                if (f.Elevator)
+                {
+                    values[counter, 4] = "Igen";
+                }
+                else
+                {
+                    values[counter, 4] = "Nem";
+                }
                 values[counter, 5] = f.NumberOfRooms;
                 values[counter, 6] = f.FloorArea;
                 values[counter, 7] = f.Price;
-                values[counter, 8] = "";
+                values[counter, 8] = "=" + GetCell(counter + 2, 8)+ "*1000000" + "/" + GetCell(counter + 2, 7);
                 counter++;
             }
 
@@ -67,7 +74,14 @@ namespace fd64yt_irf_week4
                 GetCell(2, 1),
                 GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
 
-            
+            Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+            headerRange.Font.Bold = true;
+            headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            headerRange.EntireColumn.AutoFit();
+            headerRange.RowHeight = 40;
+            headerRange.Interior.Color = Color.LightBlue;
+            headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
         }
 
         private string GetCell(int x, int y)
