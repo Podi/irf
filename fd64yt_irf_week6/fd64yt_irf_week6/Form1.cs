a5c7cc29cf0f12c3095c57653d2fe3b5ee2f14cf
podi <podipeti17@gmail.com>
2023-04-19 12:06:53 +0200

    IRF gyak6 - Labdagyár létrehozása

--- fd64yt_irf_week6/fd64yt_irf_week6/Form1.cs
+++ fd64yt_irf_week6/fd64yt_irf_week6/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using fd64yt_irf_week6.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +13,17 @@ namespace fd64yt_irf_week6
 {
     public partial class Form1 : Form
     {
+        private List<Ball> _balls = new List<Ball>();
+        /*
+        private Ballfactory _factory;
+
+        public Ballfactory Factory
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
+        */
+
         public Form1()
         {
             InitializeComponent();
