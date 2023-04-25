cb4896a4cf43f946cef6a652e3cff4ed7ce8ce71
podi <podipeti17@gmail.com>
2023-04-25 03:15:17 +0200

    IRF gyak 5 - Webszolgáltatás hívása

--- fd64yt_irf_week5/fd64yt_irf_week5/Form1.cs
+++ fd64yt_irf_week5/fd64yt_irf_week5/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using fd64yt_irf_week5.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,22 @@ namespace fd64yt_irf_week5
         public Form1()
         {
             InitializeComponent();
+            GetMnbCurrencies();
+        }
+
+        private void GetMnbCurrencies()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
