c12aa5db9266a6ab1ee9f81a90de273e6fe10113
podi <podipeti17@gmail.com>
2023-05-09 09:05:38 +0200

    IRF gyak 9 - Szimulációs lépés

--- fd64yt_irf_week9/fd64yt_irf_week9/Form1.cs
+++ fd64yt_irf_week9/fd64yt_irf_week9/Form1.cs
@@ -30,10 +30,11 @@ namespace fd64yt_irf_week9
 
             for (int year = 2005; year <= 2024; year++)
             {
-                // Végigmegyünk az összes személyen
                 for (int i = 0; i < Population.Count; i++)
                 {
-                    // Ide jön a szimulációs lépés
+                    Person p = new Person();
+                    p = Population[j];
+                    SimStep(i, p);
                 }
 
                 int nbrOfMales = (from x in Population
@@ -102,6 +103,42 @@ namespace fd64yt_irf_week9
                 }
             }
             return death;
+        }
+
+        private void SimStep(int year, Person person)
+        {
+            //Ha halott akkor kihagyjuk, ugrunk a ciklus következő lépésére
+            if (!person.IsAlive) return;
+
+            // Letároljuk az életkort, hogy ne kelljen mindenhol újraszámolni
+            byte age = (byte)(year - person.BirthYear);
+
+            // Halál kezelése
+            // Halálozási valószínűség kikeresése
+            double pDeath = (from x in DeathProbabilities
+                             where x.Gender == person.Gender && x.Age == age
+                             select x.P).FirstOrDefault();
+            // Meghal a személy?
+            if (rng.NextDouble() <= pDeath)
+                person.IsAlive = false;
+
+            //Születés kezelése - csak az élő nők szülnek
+            if (person.IsAlive && person.Gender == Gender.Female)
+            {
+                //Szülési valószínűség kikeresése
+                double pBirth = (from x in BirthProbabilities
+                                 where x.Age == age
+                                 select x.P).FirstOrDefault();
+                //Születik gyermek?
+                if (rng.NextDouble() <= pBirth)
+                {
+                    Person újszülött = new Person();
+                    újszülött.BirthYear = year;
+                    újszülött.ChildNum = 0;
+                    újszülött.Gender = (Gender)(rng.Next(1, 3));
+                    Population.Add(újszülött);
+                }
+            }
         }
     }
 }
