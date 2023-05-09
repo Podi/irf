6f28706d7aeadf3cae7a6e9764fb4cb12748de48
podi <podipeti17@gmail.com>
2023-05-09 08:45:35 +0200

    IRF gyak 9 - Osztályok létrehozása

---
+++ fd64yt_irf_week9/fd64yt_irf_week9/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fd64yt_irf_week9.Entities
+{
+    public class BirthProbability
+    {
+        public int Age { get; set; }
+        public byte ChildNum { get; set; }
+        public double P { get; set; }
+    }
+}
---
+++ fd64yt_irf_week9/fd64yt_irf_week9/Entities/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fd64yt_irf_week9.Entities
+{
+    public class Person
+    {
+        public int BirthYear { get; set; }
+        public Gender Gender { get; set; }
+        public byte ChildNum { get; set; }
+        public bool IsAlive { get; set; }
+
+        public Person()
+        {
+            IsAlive = true;
+        }
+    }
+}
